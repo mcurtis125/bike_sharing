8d320929176178ec07c1affcd2daedf44da221d5
voula11 <stavroula.theophanous@mail.mcgill.ca>
2015-03-23 15:55:12 -0400

    absolute value of log error

--- bike_kaggle_simple_local.py
+++ bike_kaggle_simple_local.py
@@ -22,22 +22,6 @@ def main():
     data_train = data[data['weekofyear'] % 2 == 0]
     data_test = data[data['weekofyear'] % 2 != 0]
     
-    """
-    plt.subplot(2, 1, 1)
-    plt.plot(data_train.index, data_train['count'], '-yo')
-    plt.title('Training Data: Datetime VS. Count')
-    plt.ylabel('Count')
-    plt.xlabel('Datetime')
-    
-    plt.subplot(2, 1, 2)
-    plt.plot(data_test.index, data_test['count'], '-yo')
-    plt.title('Training Data: Datetime VS. Count')
-    plt.ylabel('Count')
-    plt.xlabel('Datetime')
-    """
-    
-    plt.show()
-    
     #build two data sets: one for registered users and one for casual, splitting data here as appropriate
     X_selected_cols = [u'weather',u'month',u'dayofweek',u'hour',u'season',u'holiday',u'workingday',u'temp',u'atemp',u'humidity',u'windspeed']
     X_train = data_train[X_selected_cols]
@@ -70,12 +54,14 @@ def main():
     #groom for csv printing
     y_test_datetime = pd.DataFrame(data_test.index, columns=['datetime'])
     y_test_count = pd.DataFrame(y_test, dtype='float', columns=['count'])
+    y_test_weather = pd.DataFrame(data_test['weather'], dtype='int', columns=['weather'])
     y_test = pd.DataFrame()
     y_test['datetime'] = y_test_datetime['datetime']
     y_test['count'] = y_test_count['count']
+    y_test['weather'] = y_test_weather['weather'].astype(int)
 	
     #find the error for graphing
-    y_test['logerror'] = pd.Series(np.log(np.array(y_test['count'])) - np.log(np.array(data_test['count'])))
+    y_test['logerror'] = pd.Series(np.absolute(np.log(np.array(y_test['count'])) - np.log(np.array(data_test['count']))))
     y_test = y_test.set_index('datetime')
     y_test['hour'] = y_test.index.hour
     y_test['dayofweek'] = y_test.index.weekday
@@ -99,44 +85,32 @@ def main():
     plt.bar(hour_weekend.index + bar_width, hour_weekend['mean'], bar_width, yerr=hour_weekend['std'], alpha=0.4, color='g', ecolor='g', label='Weekend')
     #chart labeling
     plt.xticks(hour_workday.index + bar_width, hour_workday.index)
-    plt.ylabel('Mean Error: log(predicted) - log(actual)')
+    plt.ylabel('Mean Error: (log(predicted) - log(actual))^2')
     plt.xlabel('Hour in Day')
     plt.title('Training Data: Hour in Day VS. Mean Error on Hour')
     plt.legend()
     plt.show()
-    
-    """
-	#plot squared log error by day
-    error = np.exp( (np.log(np.array(data_test['count']) + 1) - np.log(np.array(y_test['count']) + 1)), 2 )
-    plt.subplot(2, 1, 1)
-    plt.plot(y_test['datetime'], error, '-yo')
-    plt.title('Error vs time')
-    plt.ylabel('Squared Log Error')
-    plt.xlabel('Datetime')
-    
-    #plot number of errors by time of day (# errors above a threshold)
-    plt.subplot(2, 1, 2)
-    error_hour = np.zeros(24)
-    for i in range(0,error.size-1):
-        if error[i] > 2:
-            error_hour[data_test['hour'][i]] = error_hour[data_test['hour'][i]]+1
-    x = np.arange(0, 24, 1)
-    bar_width = 1
-    plt.bar(x, error_hour, 1, color='y', ecolor='y', label='#Errors > 0.5')
-    plt.xticks(x)
-    plt.title('Error vs hour of the day')
-    plt.ylabel('# Errors > 2')
-    plt.xlabel('Hour of the day')
 
+'''
+    #plot by hour average count for each hour in the day on weekend and and weekdays
+    hour_workday = y_test[(y_test['dayofweek'] < 5)].groupby('weather').agg({'logerror': [np.mean, np.std]})
+    hour_weekend = y_test[(y_test['dayofweek'] > 4)].groupby('weather').agg({'logerror': [np.mean, np.std]})
+    #droplevel statement just allows access to the mean and std columns
+    hour_workday.columns = hour_workday.columns.droplevel(0)
+    hour_weekend.columns = hour_weekend.columns.droplevel(0)
+    plt.figure().canvas.set_window_title('Training Data: Weather Type VS. Mean Error on Hour')
+    #plot each bar
+    bar_width = 0.4
+    plt.bar(hour_workday.index, hour_workday['mean'], bar_width, yerr=hour_workday['std'], alpha=0.4, color='b', ecolor='b', label='Workday')
+    plt.bar(hour_weekend.index + bar_width, hour_weekend['mean'], bar_width, yerr=hour_weekend['std'], alpha=0.4, color='g', ecolor='g', label='Weekend')
+    #chart labeling
+    plt.xticks(hour_workday.index + bar_width, hour_workday.index)
+    plt.ylabel('Mean Error: (log(predicted) - log(actual))^2')
+    plt.xlabel('Weather Type')
+    plt.title('Training Data: Weather Type VS. Mean Error on Hour')
+    plt.legend()
     plt.show()
-	
-    
-	#print root mean squared log error
-    print np.sqrt(np.sum(error)/error.size)
-    
-    #printing to csv
-    y_test.to_csv('simple_output.csv', columns=['datetime', 'count'], index=0)
-	"""
+'''
 
 if __name__=="__main__":
     main()
